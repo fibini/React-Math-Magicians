bb26bebeebf43093639449f12ed5f3987ef33936
Fabien <fabienswift@gmail.com>
2022-06-28 19:44:21 -0400

    importcalculator.js into app.js and not index.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+class App extends React.PureComponent {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
