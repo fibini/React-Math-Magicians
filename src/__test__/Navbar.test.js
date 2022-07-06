658fa2bbb62366c93c7426077e1ce5f600562c3f
Daf <dafne.azzolina@gmail.com>
2022-07-06 18:22:38 -0300

    Make tests for calculate.js and Navbar.js

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,23 +1,22 @@
-// test with react
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App';
 
 describe('test Navbar', () => {
-  test('renders learn react link', () => {
+  test('renders home page', () => {
     render(<App />);
     fireEvent.click(screen.getByText(/Home/));
     expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
   });
 
-  test('renders learn react link', () => {
+  test('renders calculator page', () => {
     render(<App />);
     fireEvent.click(screen.getByText(/Calculator/));
     expect(screen.getByText(/some/i)).toBeInTheDocument();
   });
 
-  test('renders learn react link', () => {
+  test('renders quote page', () => {
     render(<App />);
     fireEvent.click(screen.getByText(/Quote/));
     expect(screen.getByText(/William/i)).toBeInTheDocument();
--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,11 +1,16 @@
-import calculate from '../__mocks__/calculate';
+import calculate from './calculate';
 
 describe('test for calculate.js', () => {
-  test('calculates properly', () => {
-    expect(calculate('0', 'AC')).toStrictEqual({
-      total: null,
-      next: null,
-      operation: null,
-    });
+  test('tests the AC button', () => {
+    expect(calculate({ total: '10', next: null, operation: null }, 'AC')).toStrictEqual({ total: null, next: null, operation: null });
+  });
+  test('tests the = button', () => {
+    expect(calculate({ total: '10', next: '4', operation: 'x' }, '=')).toStrictEqual({ total: '40', next: null, operation: null });
+  });
+  test('tests "."', () => {
+    expect(calculate({ total: '10', next: null, operation: null }, '.')).toStrictEqual({ total: '10', next: '10.', operation: null });
+  });
+  test('tests the +/- button', () => {
+    expect(calculate({ total: '8', next: null, operation: null }, '+/-')).toStrictEqual({ total: '-8', next: null, operation: null });
   });
 });
