abf145c33f544c6348eebdace167e24cd72d086b
Fabien <fabienswift@gmail.com>
2022-07-17 12:55:23 -0400

    fix test file import links

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Home from '../pages/Home';
+import Home from '../components/pages/Home';
 
 describe('Jest Snapshot testing suite', () => {
   it('Matches DOM Snapshot', () => {
